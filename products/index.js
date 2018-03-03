1b01b6fb61677d332f3cd8c6a31d7e11b2fac58a
Jamie Barton <hi@jamiebarton.co.uk>
2018-03-03 21:20:53 +0000

    Add microrouter to products example

--- products/index.js
+++ products/index.js
@@ -1,1 +1,11 @@
-module.exports = () => 'Welcome to the products API';
+const { json, send } = require('micro');
+const { router, get, post } = require('microrouter');
+
+module.exports = router(
+  get('/', (req, res) => 'Welcome to the products API'),
+  post('/', async (req, res) => {
+    const body = await json(req);
+    send(res, 200, body);
+  }),
+  get('/:id', (req, res) => req.params)
+);
